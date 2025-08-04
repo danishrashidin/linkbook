a0c88612e40f4515f0676bbf9032bdd05c76a7e7
danishrashidin <65647228+danishrashidin@users.noreply.github.com>
2025-08-04 22:49:48 +0800

    feat: add URL validation

--- backend/src/schema/link/resolvers/Mutation/addLink.ts
+++ backend/src/schema/link/resolvers/Mutation/addLink.ts
@@ -1,12 +1,20 @@
 import type { MutationResolvers } from "./../../../types.generated";
 import { PrismaClient } from "../../../../../generated/prisma";
-export const addLink: NonNullable<MutationResolvers['addLink']> = async (
+import validator from "validator";
+export const addLink: NonNullable<MutationResolvers["addLink"]> = async (
   _parent,
   _arg,
   _ctx
 ) => {
   const prisma = new PrismaClient();
   const { input } = _arg;
+
+  // Validate link
+  const isLinkValid = validator.isURL(input.url, {
+    require_protocol: true,
+  });
+  if (!isLinkValid) throw "URL is not a valid link";
+
   return await prisma.link.create({
     data: {
       url: input.url,
